dd3787c10e532c2719d4be8408022bf065a51d5d
maxmalkhazov <maxmal86@gmail.com>
2017-10-24 20:54:01 -0500

    Initial commit

---
+++ resources/js/script.js
@@ -0,0 +1,137 @@
+
+/* 
+var x = document.getElementsByTagName("h1");
+for (var i = 0; i < x.length; i++) {
+	x[i].style.color = "red";
+}
+*/
+
+
+$(document).ready(function() {
+
+
+/* FOR THE STICKY NAVIGATION */
+
+	$('.js--section-features').waypoint(function(direction) {
+		if (direction === "down") {
+			$('nav').addClass('sticky');
+		}
+		else {
+			$('nav').removeClass('sticky');
+		}
+	}, {
+		offset: '60px'
+	});
+
+/* SCROLL ON BUTTONS */
+
+    $('.js--scroll-to-plans').click(function () {
+    	$('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+
+    });
+
+    $('.js--scroll-to-start').click(function () {
+    	$('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+
+    });
+
+/* NAVIGATION SCROLLING */
+
+   // Select all links with hashes
+	$('a[href*="#"]')
+	  // Remove links that don't actually link to anything
+	  .not('[href="#"]')
+	  .not('[href="#0"]')
+	  .click(function(event) {
+	    // On-page links
+	    if (
+	      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+	      && 
+	      location.hostname == this.hostname
+	    ) {
+	      // Figure out element to scroll to
+	      var target = $(this.hash);
+	      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+	      // Does a scroll target exist?
+	      if (target.length) {
+	        // Only prevent default if animation is actually gonna happen
+	        event.preventDefault();
+	        $('html, body').animate({
+	          scrollTop: target.offset().top
+	        }, 1000, function() {
+	          // Callback after animation
+	          // Must change focus!
+	          var $target = $(target);
+	          $target.focus();
+	          if ($target.is(":focus")) { // Checking if the target was focused
+	            return false;
+	          } else {
+	            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+	            $target.focus(); // Set focus again
+	          };
+	        });
+	      }
+	    }
+	});
+
+/* ANIMATION ON SCROLL */
+
+	$('.js--wp-1').waypoint(function(direction) {
+	 $('.js--wp-1').addClass('animated fadeIn');
+	}, {
+	   offset: '50%'
+	});
+	$('.js--wp-2').waypoint(function(direction) {
+	 $('.js--wp-2').addClass('animated fadeInUp');
+	}, {
+	   offset: '50%'
+	});
+	$('.js--wp-3').waypoint(function(direction) {
+	 $('.js--wp-3').addClass('animated fadeIn');
+	}, {
+	   offset: '50%'
+	});
+	$('.js--wp-4').waypoint(function(direction) {
+	 $('.js--wp-4').addClass('animated pulse');
+	}, {
+	   offset: '50%'
+	});
+
+	/* MOBILE NAVIGATION */
+
+	$('.js--nav-icon').click(function() {
+		var nav = $('.js--main-nav');
+		var icon = $('.js--nav-icon i');
+
+		nav.slideToggle(200);
+		if (icon.hasClass('ion-navicon-round')) {
+			icon.addClass('ion-close-round');
+			icon.removeClass('ion-navicon-round');
+		}
+		else {
+			icon.removeClass('ion-close-round');
+			icon.addClass('ion-navicon-round');
+		}
+
+	});
+
+	/* MAPS */
+
+	var map = new GMaps({
+	  div: '.map',
+	  lat: 41.8814659,
+	  lng: -87.5987966,
+	  zoom: 13
+	});
+
+	map.addMarker({
+	  lat: 41.882065,
+	  lng: -87.6300497,
+	  title: 'Chicago',
+	  infoWindow: {
+		  content: '<p>Our Chicago HQ</p>'
+		}
+	});
+
+});
+
